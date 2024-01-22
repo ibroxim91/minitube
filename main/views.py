c916e32c2227fffef8c67d13fe259b561b48f608
ibrohim <ibroxim.rashidov@yandex.ru>
2024-01-22 17:23:14 +0500

    sdsdf

--- main/views.py
+++ main/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from .models import VideoContent
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "index.html")
+        page = 1
+        if request.GET.get("page"):
+            page = int(request.GET.get("page"))
+        videos = VideoContent.objects.all()
+        paginator = Paginator(videos , 1)
+        page_objects = paginator.get_page( page )  
+        print()  
+        print(page_objects.object_list)  
+        print()  
+        context = {'videos': page_objects.object_list ,"page_objects":page_objects, 'paginator': paginator}
+        return render(request, "index.html", context )
 
